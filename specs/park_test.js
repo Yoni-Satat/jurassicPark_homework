11bb262923e69ccc93e75c277917cd31b4e572f8
Yoni Satat <yoni.satat@gmail.com>
2017-12-05 16:51:56 +0000

    4 tests passed

--- specs/park_test.js
+++ specs/park_test.js
@@ -12,4 +12,9 @@ describe('Park', function(){
   it('should start with empty enclosure', function(){
     assert.strictEqual(park.enclosure.length, 0);
   });
+
+  it('should have one dinosaur in the enclosure', function(){
+    park.addDinosaur(dinosaur1);
+    assert.strictEqual(park.enclosure.length, 1);
+  });
 });
